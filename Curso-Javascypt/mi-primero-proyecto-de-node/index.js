5e6415fa36ef8804720a36806392b99336a8cf90
PanaLennyFS <117951425+PanaLennyFS@users.noreply.github.com>
2022-11-10 16:28:16 -0600

    Add files via upload

---
+++ Curso-Javascypt/mi-primero-proyecto-de-node/index.js
@@ -0,0 +1,11 @@
+// Esta es el mensaje de bienvenida
+console.log("Esta es la puerta de entrada al proyecto");
+
+/*
+Esta es la parte intermedia de nuestro proyecto
+En esta parte realizamos los chequeos necesarios
+poe último vamos a la ulima linea de saludo
+*/
+
+// Este va a ser el mensaje que se envia para despedir
+console.log("Adios")
